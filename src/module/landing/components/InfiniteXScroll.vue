816410c950fb9b8e2631f4be62acea2e7bd2cbac
Jeison Ferrer <jeison120120@hotmail.com>
2024-10-18 21:28:11 -0500

    separando el InfiniteXScroll de Home page a un componente propio

---
+++ src/module/landing/components/InfiniteXScroll.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="mt-7 scrolling-content">
+    <div class="slide-track">
+      <div v-for="logo in logos" :key="logo" class="store-div">
+        <img :src="logo" alt="Store logo" />
+      </div>
+      <div v-for="logo in logos" :key="logo + '2'" class="store-div">
+        <img :src="logo" alt="Store logo" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const StoreLogo = {
+  FARMATODO:
+    'https://revistabusinessvenezuela.com/webv/wp-content/uploads/2021/04/LOGO-FARMATODO.jpg',
+  BELLA_PIEL:
+    'https://eltesoro.com.co/wp-content/uploads/2020/07/bella-piel-logo-el-tesoro-1.jpg',
+  CRUZ_VERDE:
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Logotipo_Cruz_Verde.svg/800px-Logotipo_Cruz_Verde.svg.png',
+  MEDIPIEL:
+    'https://www.laquinta.com.co/wp-content/uploads/2022/11/Medipiel.png',
+}
+
+const logos = Object.values(StoreLogo)
+</script>
+
+<style scoped>
+.scrolling-content {
+  width: 300px;
+  height: auto;
+  overflow: hidden;
+  /* 6 elementos de 100px de ancho */
+}
+.scrolling-content .slide-track {
+  display: flex;
+  animation: scroll 10s linear infinite;
+  -webkit-animation: scroll 10s linear infinite;
+  width: calc(70px * 8);
+}
+
+.scrolling-content .slide-track .store-div {
+  width: 70px;
+}
+.slide-track .store-div img {
+  width: 100%;
+}
+
+@keyframes scroll {
+  0% {
+    -webkit-transform: translateX(0);
+    transform: translateX(0);
+  }
+  100% {
+    -webkit-transform: translateX(calc(-70px * 4));
+    transform: translateX(calc(-70px * 4));
+  }
+}
+</style>
--- src/module/landing/pages/HomePage.vue
+++ src/module/landing/pages/HomePage.vue
@@ -11,58 +11,7 @@
           <span class="colombia-gradient font-bold">pais</span>.
         </h2>
       </div>
-      <div class="mt-7 scrolling-content">
-        <div class="slide-track">
-          <div class="store-div">
-            <img
-              src="https://revistabusinessvenezuela.com/webv/wp-content/uploads/2021/04/LOGO-FARMATODO.jpg"
-              alt=""
-            />
-          </div>
-          <div class="store-div">
-            <img
-              src="https://eltesoro.com.co/wp-content/uploads/2020/07/bella-piel-logo-el-tesoro-1.jpg"
-              alt=""
-            />
-          </div>
-          <div class="store-div">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Logotipo_Cruz_Verde.svg/800px-Logotipo_Cruz_Verde.svg.png"
-              alt=""
-            />
-          </div>
-          <div class="store-div">
-            <img
-              src="https://www.laquinta.com.co/wp-content/uploads/2022/11/Medipiel.png"
-              alt=""
-            />
-          </div>
-          <div class="store-div">
-            <img
-              src="https://revistabusinessvenezuela.com/webv/wp-content/uploads/2021/04/LOGO-FARMATODO.jpg"
-              alt=""
-            />
-          </div>
-          <div class="store-div">
-            <img
-              src="https://eltesoro.com.co/wp-content/uploads/2020/07/bella-piel-logo-el-tesoro-1.jpg"
-              alt=""
-            />
-          </div>
-          <div class="store-div">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Logotipo_Cruz_Verde.svg/800px-Logotipo_Cruz_Verde.svg.png"
-              alt=""
-            />
-          </div>
-          <div class="store-div">
-            <img
-              src="https://www.laquinta.com.co/wp-content/uploads/2022/11/Medipiel.png"
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
+      <InfiniteXScroll />
     </section>
 
     <section class="flex flex-col text-gray max-w-5xl px-2">
@@ -72,6 +21,7 @@
 </template>
 
 <script setup lang="ts">
+import InfiniteXScroll from '../components/InfiniteXScroll.vue'
 import SearchInput from '../components/SearchInput.vue'
 </script>
 
@@ -88,38 +38,4 @@ import SearchInput from '../components/SearchInput.vue'
   background-clip: text;
   text-fill-color: transparent;
 }
-
-.scrolling-content {
-  width: 300px;
-  height: auto;
-
-  overflow: hidden;
-  /* 6 elementos de 100px de ancho */
-}
-.scrolling-content .slide-track {
-  display: flex;
-  animation: scroll 10s linear infinite;
-  -webkit-animation: scroll 10s linear infinite;
-  width: calc(70px * 8);
-}
-
-.scrolling-content .slide-track .store-div {
-  width: 70px;
-}
-.slide-track .store-div img {
-  width: 100%;
-}
-
-@keyframes scroll {
-  0% {
-    -webkit-transform: translateX(0);
-    transform: translateX(0);
-  }
-  100% {
-    -webkit-transform: translateX(calc(-70px * 4));
-    transform: translateX(calc(-70px * 4));
-  }
-}
 </style>
-
-/** */
