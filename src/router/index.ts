df832d7d1716c81d8042d961340b02b04bb17a90
Jeison Ferrer <jeison120120@hotmail.com>
2024-10-18 22:06:03 -0500

    prueba de rutas en vercel

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,7 @@ const router = createRouter({
       component: import('@/module/landing/layouts/LandingLayout.vue'),
       children: [
         {
-          path: '',
+          path: '/home',
           name: 'home',
           component: import('@/module/landing/pages/HomePage.vue'),
         },
